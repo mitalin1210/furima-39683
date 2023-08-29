0a0330274ff24816131245ae42ff8ba89f317585
mitalin1210 <141559728+mitalin1210@users.noreply.github.com>
2023-08-29 09:17:43 +0900

    firstcommit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'mitalin' && password == '1210'
+    end
+  end
+end
+end
