ffe6d892af70eac792b68c41e81056b6d63ea1b6
mitalin1210 <141559728+mitalin1210@users.noreply.github.com>
2023-09-04 02:18:01 +0900

    商品出品機能レビューコメントの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
-class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
-  def index
-    @items = Item.includes(:user).order('created_at DESC')
-  end
+class ItemsController < ApplicationController  
+  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :set_item, only: [:edit, :show, :update, :destroy]
+  # def index
+  #   @items = Item.includes(:user).order('created_at DESC')
+end
 
   def new
     @item = Item.new
@@ -17,7 +17,6 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-end 
 
 
 private
@@ -26,6 +25,6 @@ def item_params
   params.require(:item).permit(:image, :product_name, :description, :selling_price, :genre_id, :genre_status_id, :genre_shipping_cost_id, :genre_prefecture_id, :genre_shipping_day_id).merge(user_id: current_user.id)
 end
 
-def set_item
-  @item = Item.find(params[:id])
-end
+# def set_item
+#   @item = Item.find(params[:id])
+# end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :genre_shipping_cost
   belongs_to_active_hash :genre_shipping_day
   belongs_to_active_hash :genre_status
-  belongs_to_active_hash :genre
-  belongs_to_active_hash :genre_prefecture
-  belongs_to :user, optional: true
-  has_one :order
+  # belongs_to_active_hash :genre
+  # belongs_to_active_hash :genre_prefecture
+  # belongs_to :user
+  # has_one :order
   
 
   with_options presence: true do
     validates :image
     validates :product_name
     validates :description
-    validates :selling_price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 299, less_than_or_equal_to: 999_999_999 }
+    validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 299, less_than_or_equal_to: 999_999_999 }
     validates :genre_shipping_cost_id
     validates :genre_shipping_day_id
     validates :genre_status_id
