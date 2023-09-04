b452177d9f4acf4762ef416148ae87d4eace4906
mitalin1210 <141559728+mitalin1210@users.noreply.github.com>
2023-09-05 00:20:21 +0900

    商品一覧表示機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController  
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, only: [:edit, :show, :update, :destroy]
-  # def index
-  #   @items = Item.includes(:user).order('created_at DESC')
-  # end
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
 
   def new
     @item = Item.new
@@ -25,7 +26,7 @@ class ItemsController < ApplicationController
   params.require(:item).permit(:image, :product_name, :description, :selling_price, :genre_id, :genre_status_id, :genre_shipping_cost_id, :genre_prefecture_id, :genre_shipping_day_id).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
