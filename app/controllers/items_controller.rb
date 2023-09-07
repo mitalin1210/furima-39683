0f70ebca8d03b93bcad63755728f0c0d52347b84
mitalin1210 <141559728+mitalin1210@users.noreply.github.com>
2023-09-07 11:15:15 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,15 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
 
-
-
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
-
-
-  
-
-
-
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -58,6 +49,16 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    # ログインしているユーザーと同一であればデータを削除する
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
